d6abdf19dc16c40f3d72230f8788ed6c8ab7d764
ChiragPadyal <freakstar03@gmail.com>
2022-04-14 15:52:53 +0530

    Updates

---
+++ precache-manifest.89e4b288428545d3c394fceb072df4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd46b95e0e430bb7a97c911cacbd0ac",
+    "url": "/React_Alan_Ai_News_App/index.html"
+  },
+  {
+    "revision": "eea3cbcdcd50cf099608",
+    "url": "/React_Alan_Ai_News_App/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "1048dd33d5461f5350b7",
+    "url": "/React_Alan_Ai_News_App/static/js/2.4e97a4fc.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/React_Alan_Ai_News_App/static/js/2.4e97a4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea3cbcdcd50cf099608",
+    "url": "/React_Alan_Ai_News_App/static/js/main.68ea4629.chunk.js"
+  },
+  {
+    "revision": "e120fae867d2d956b401",
+    "url": "/React_Alan_Ai_News_App/static/js/runtime-main.7d909671.js"
+  }
+]);
